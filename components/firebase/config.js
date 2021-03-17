b6771f0209112ef6bb29142d8a7c36693ac3260a
kunal-j10 <kunalsinghlegal@gmail.com>
2021-03-17 09:30:07 +0530

    firebase config hidden

--- components/firebase/config.js
+++ components/firebase/config.js
@@ -4,13 +4,13 @@ import 'firebase/firestore';
 // Your web app's Firebase configuration
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
   var firebaseConfig = {
-    apiKey: "AIzaSyDaX_9h7Y3fL8hwxuOY--D7Aj2kt7PhBrc",
-    authDomain: "imagegram-6e43c.firebaseapp.com",
-    projectId: "imagegram-6e43c",
-    storageBucket: "imagegram-6e43c.appspot.com",
-    messagingSenderId: "537670226474",
-    appId: "1:537670226474:web:ddbe4f2b16754f43f02953",
-    measurementId: "G-C314L9XKN5"
+    apiKey: "***********",
+    authDomain: "***********",
+    projectId: "***********",
+    storageBucket: "***********",
+    messagingSenderId: "***********",
+    appId: "***********",
+    measurementId: "***********"
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
