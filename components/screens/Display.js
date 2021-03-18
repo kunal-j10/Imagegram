47dfd8087a178ba55231ca0c7cadb2c9553cebf1
kunal-j10 <kunalsinghlegal@gmail.com>
2021-03-18 22:35:27 +0530

    Added storage imgae uri to firestore

--- components/screens/Display.js
+++ components/screens/Display.js
@@ -1,34 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View,Text,Image, Button } from 'react-native';
 import ProgressBar from './progressBar';
 import firebase from 'firebase';
-import {projectStorage} from '../firebase/config';
+import {projectStorage,projectFirestore} from '../firebase/config';
 require("firebase/firestore")
 require("firebase/firebase-storage")
 
 
-const Display = ({uri,fileName,fileType})=>{
+const Display = ({uri,fileName,fileType,imageName,fileSize})=>{
     const file={
         name:fileName,
         uri:uri,
-        type:fileType
+        type:fileType,
+        imageName:imageName
     }
+    const [percentage,setPercentage]= useState(null);
+
     const saveHandler= async ()=>{
         const response = await fetch(file.uri);
         const blob = await response.blob();
 
         const task = projectStorage
                      .ref()
-                     .child(`posts/${fileName}`)
+                     .child(`posts/${imageName}`)
                      .put(blob)
         
         const taskProgress = snapshot =>{
-            console.log(`transferred: ${snapshot.bytesTransferred}`)
+            console.log(`transferred: ${(snapshot.bytesTransferred/snapshot.totalBytes)*100}`)
+            setPercentage((snapshot.bytesTransferred/snapshot.totalBytes)*100);
         }
 
         const taskCompleted = () =>{
             task.snapshot.ref.getDownloadURL().then((snapshot)=>{
                 console.log(snapshot)
+                savePostData(snapshot);
             })
         }
         const taskError = snapshot=>{
@@ -37,15 +42,30 @@ const Display = ({uri,fileName,fileType})=>{
 
         task.on("state_changed",taskProgress,taskError,taskCompleted);
     }
+    
+    const savePostData=(DownloadURL)=>{
+        projectFirestore.collection('posts')
+                        .add({
+                            url: DownloadURL,
+                            name:imageName,
+                            //creation: projectFirestore.FieldValue.serverTimestamp()
+                        })
+                        .then(()=>{
+                            console.log("added on firestore")
+                        })
+    }
+
     return(
         <View style={{margin:20}}>
             <View style={{backgroundColor:'red',height:400}}>
                 <Image source={{uri:uri}} style={{flex:1}}/>
             </View>
             <View>
-             <Text>Image name :- {fileName}</Text> 
-             <Text>Image type :- {fileType}</Text>  
-             {/* <ProgressBar file={file}/> */}
+             <Text style={{color:'red'}}>{fileName}</Text> 
+             <Text>size:- {(fileSize)/1048576} mb</Text>
+             <View>
+             <ProgressBar percentage={percentage}/>
+             </View>
              <Button onPress={saveHandler}
                      title={'Save post'}/>
             </View>
